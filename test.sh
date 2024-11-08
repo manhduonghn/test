cee8715ca44342f83fdcf2576c2aa4df8970e7c5
manhduonghn <manhduonghn@live.com>
2024-11-08 07:22:12 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+req() {
+    wget --header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0" \
+         --header="Content-Type: application/octet-stream" \
+         --header="Accept-Language: en-US,en;q=0.9" \
+         --header="Connection: keep-alive" \
+         --header="Upgrade-Insecure-Requests: 1" \
+         --header="Cache-Control: max-age=0" \
+         --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" \
+         --keep-session-cookies --timeout=30 -nv -O "$@"
+}
+
+download_resources() {
+    for repo in revanced-patches revanced-cli; do
+        githubApiUrl="https://api.github.com/repos/revanced/$repo/releases"
+        page=$(req - 2>/dev/null $githubApiUrl)
+        assetUrls=$(echo $page | jq -r '.assets[] | select(.name | endswith(".asc") | not) | "\(.browser_download_url) \(.name)"')
+        while read -r downloadUrl assetName; do
+            req "$assetName" "$downloadUrl" 
+        done <<< "$assetUrls"
+    done
+}
+
+download_resources
