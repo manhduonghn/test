16e476d1992d64b4c29e05fd007b6f9142286a2c
manhduonghn <manhduonghn@live.com>
2024-11-08 08:20:34 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -39,6 +39,6 @@ find_max_version() {
 
 download_resources
 
-version=$(find_max_version "com.facebook.orca")
+version=$(find_max_version "com.google.android.apps.youtube.music")
 
 echo "$version"
