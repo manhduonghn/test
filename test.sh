9f5c46af91c20378bf1571acde5f302b0ce159b8
manhduonghn <manhduonghn@live.com>
2024-11-08 08:34:03 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -25,8 +25,8 @@ find_max_version() {
     package_name=$1
     output=$(java -jar revanced-cli*.jar list-versions -f "$package_name" patch*.rvp)
     echo "$output"
-    versions=$(echo "$output" | tail -n +3 | sed 's/ (.*)//')
-    echo $versions
+    versions=$(echo "$output" | tail -n +3 | sed 's/ (.*)//' | grep -v -w "Any")
+    echo "$versions"
 
     max_version=$(echo "$versions" | sort -V | tail -n 1)
     echo "$max_version"
