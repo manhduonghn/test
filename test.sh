f572adbc885f3e0d60c03545b6ffb167fee8d92d
manhduonghn <manhduonghn@live.com>
2024-11-08 07:49:46 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 req() {
     wget --header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0" \
          --header="Content-Type: application/octet-stream" \
@@ -22,4 +23,22 @@ download_resources() {
 
 download_resources
 
-java -jar revanced-cli*.jar list-versions -f com.google.android.youtube patch*.rvp
+# Chạy lệnh và lưu kết quả vào biến
+output=$(java -jar revanced-cli*.jar list-versions -f com.google.android.youtube patch*.rvp)
+
+# Trích xuất các phiên bản từ kết quả của lệnh
+versions=$(echo "$output" | grep -oP '\d+(\.\d+)*([.-][a-zA-Z0-9]+)*')
+# Hàm so sánh hai phiên bản và trả về phiên bản cao nhất
+compare_versions() {
+    printf "%s\n%s" "$1" "$2" | sort -V | tail -n 1
+}
+
+# Khởi tạo phiên bản cao nhất là phần tử đầu tiên
+max_version=$(echo "$versions" | head -n 1)
+
+# Lặp qua các phiên bản còn lại và tìm phiên bản cao nhất
+while read -r version; do
+    max_version=$(compare_versions "$max_version" "$version")
+done <<< "$versions"
+
+echo "Phiên bản cao nhất là: $max_version"
