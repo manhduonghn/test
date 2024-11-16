eb6b7e67028ed0aa7882ce21c4d0cd93a8bbd3e4
manhduonghn <manhduonghn@live.com>
2024-11-16 22:30:40 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
+req() {
+    wget --header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0" \
+         --header="Content-Type: application/octet-stream" \
+         --header="Accept-Language: en-US,en;q=0.9" \
+         --header="Connection: keep-alive" \
+         --header="Upgrade-Insecure-Requests: 1" \
+         --header="Cache-Control: max-age=0" \
+         --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" \
+         --keep-session-cookies --timeout=30 -nv -O "$@"
+}
 
+url="https://youtube.en.uptodown.com/android/versions"
+
+text =$(req - $url | grep 'detail-app-name')
+echo $text
