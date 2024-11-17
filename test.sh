b229527219c81588daf37aa0f03431b936b7c1b0
manhduonghn <manhduonghn@live.com>
2024-11-17 07:29:01 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -26,6 +26,26 @@ get_latest_version() {
     grep -Evi 'alpha|beta' | grep -oPi '\b\d+(\.\d+)+(?:\-\w+)?(?:\.\d+)?(?:\.\w+)?\b' | max
 }
 
+# Read highest supported versions from Revanced 
+get_supported_version() {
+    package_name=$1
+    output=$(java -jar revanced-cli*.jar list-versions -f "$package_name" patch*.rvp)
+    version=$(echo "$output" | tail -n +3 | sed 's/ (.*)//' | grep -v -w "Any" | max | xargs)
+    echo "$version"
+}
+
+# Download necessary resources to patch from Github latest release 
+download_resources() {
+    for repo in revanced-patches revanced-cli ; do
+        githubApiUrl="https://api.github.com/repos/revanced/$repo/releases/latest"
+        page=$(req - 2>/dev/null $githubApiUrl)
+        assetUrls=$(echo $page | jq -r '.assets[] | select(.name | endswith(".asc") | not) | "\(.browser_download_url) \(.name)"')
+        while read -r downloadUrl assetName; do
+            req "$assetName" "$downloadUrl" 
+        done <<< "$assetUrls"
+    done
+}l
+
 # Function to download a specific APK version
 uptodown() {
     config_file="./apps/uptodown/$1.json"
@@ -100,5 +120,6 @@ uptodown() {
 }
 
 # Example usage
+download_resources
 uptodown "youtube"
 uptodown "youtube-music"
