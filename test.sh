b049335fe628714530f620400b340d092ff1ad3c
manhduonghn <manhduonghn@live.com>
2024-11-17 20:17:13 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -49,7 +49,7 @@ dpi="nodpi"
 page_content=$(req "$url")
 
 # Lọc nội dung từ `</a class="accent_color"` đến `>nodpi<`
-filtered_content=$(echo "$page_content" | filter_lines_reverse '</a class="accent_color"' ">\s*${dpi}\s*<")
+filtered_content=$(echo "$page_content" | filter_lines_reverse '</a class="accent_color"' ">'${dpi}'<")
 
 # In kết quả
 echo "$filtered_content"
