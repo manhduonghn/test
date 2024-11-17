4f80f45e72e5b2998c955417ae17a457dfda6e38
manhduonghn <manhduonghn@live.com>
2024-11-17 20:44:45 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,44 +12,10 @@ req() {
          --keep-session-cookies --timeout=30 -nv -O "$@"
 }
 
-# Hàm filter_lines (lấy từ `</a class="accent_color"` gần nhất tới `>nodpi<`)
-filter_lines() {
-    local start_pattern="</a class=\"accent_color\""
-    local end_pattern=">nodpi<"
-    local buffer=()
-    local found=0
+url="https://www.apkmirror.com/apk/google-inc/chrome/chrome-131-0-6778-39-release/"
+url=$(req - $url | perl -ne 'push @buffer, $_; if (/>\s*'$dpi'\s*</) { print @buffer[-16..-1]; @buffer = (); }' \
+                 | perl -ne 'push @buffer, $_; if (/>\s*'$arch'\s*</) { print @buffer[-14..-1]; @buffer = (); }' \
+                 | perl -ne 'push @buffer, $_; if (/>\s*APK\s*</) { print @buffer[-6..-1]; @buffer = (); }' \
+                 | perl -ne 'print "https://www.apkmirror.com$1\n" if /.*href="(.*apk-[^"]*)".*/ && ++$i == 1;')
 
-    # Duyệt qua từng dòng
-    while IFS= read -r line; do
-        # Nếu khớp với dòng chứa `end_pattern`, bật chế độ thu thập
-        if [[ $line =~ $end_pattern ]]; then
-            found=1
-        fi
-
-        # Nếu đang thu thập, lưu dòng vào buffer
-        if [[ $found -eq 1 ]]; then
-            buffer+=("$line")
-        fi
-
-        # Nếu gặp dòng chứa `start_pattern`, dừng thu thập
-        if [[ $found -eq 1 && $line =~ $start_pattern ]]; then
-            break
-        fi
-    done
-
-    # In các dòng đã thu thập (ngược thứ tự)
-    printf "%s\n" "${buffer[@]}" | tac
-}
-
-# URL để tải nội dung
-url="https://www.apkmirror.com/apk/google-inc/youtube-music/youtube-music-7-27-53-release/"
-dpi="nodpi"
-
-# Lấy nội dung trang
-page_content=$(req - "$url")
-
-# Lọc nội dung từ `</a class="accent_color"` đến `>nodpi<`
-filtered_content=$(echo "$page_content" | grep '>nodpi<' -B15)
-
-# In kết quả
-echo "$filtered_content"
+echo "$url"
