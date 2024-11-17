69a408aa62209691aca3034996745387c2eaea93
manhduonghn <manhduonghn@live.com>
2024-11-17 07:22:57 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -60,8 +60,8 @@ uptodown() {
 
         # Look for the target version
         local version_url
-        version_url=$(echo "$json" | jq -r --arg version "$version" '.[] | select(.version == $version and .kindFile == "apk") | .versionURL' | head -n 1)
-
+        version_url=$(echo "$json" | jq -r --arg version "$version" '.data | map(select(.version == $version and .kindFile == "apk")) | .[0] | .versionURL')
+	
         if [ -n "$version_url" ]; then
             echo "Found versionURL: $version_url"
             local download_url
