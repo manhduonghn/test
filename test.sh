54afe9a7003673eb78ffc938976d69e6eb0accb3
manhduonghn <manhduonghn@live.com>
2024-11-18 21:36:59 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -61,10 +61,8 @@ get_apkmirror_version() {
 }
 
 # URL cần tải
-url="https://www.apkmirror.com/uploads/?appcategory=twitter"
+url="https://www.apkmirror.com/uploads/?appcategory=youtube-music"
 version="$(req - $url | grep -oP 'class="fontBlack"[^>]*href="[^"]+"\s*>\K[^<]+' | sed 20q | grep -Evi 'alpha|beta' | awk '{print $NF}' | max)"
-echo "$version"
-exit
 url="https://www.apkmirror.com/apk/google-inc/youtube-music/youtube-music-${version//./-}-release/"
 url="https://www.apkmirror.com$(req - "$url" | extract_filtered_links "nodpi" "arm64-v8a" "APK")"
 url="https://www.apkmirror.com$(req - "$url" | grep -oP 'class="[^"]*downloadButton[^"]*"[^>]*href="\K[^"]+')"
