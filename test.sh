7b35ab1384cec8552b6988efe01d3324d738039d
manhduonghn <manhduonghn@live.com>
2024-11-18 16:20:49 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,27 @@
+req() {
+    wget --header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0" \
+         --header="Content-Type: application/octet-stream" \
+         --header="Accept-Language: en-US,en;q=0.9" \
+         --header="Connection: keep-alive" \
+         --header="Upgrade-Insecure-Requests: 1" \
+         --header="Cache-Control: max-age=0" \
+         --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" \
+         --keep-session-cookies --timeout=30 -nv -O "$@"
+}
+
+max() {
+	local max=0
+	while read -r v || [ -n "$v" ]; do
+		if [[ ${v//[!0-9]/} -gt ${max//[!0-9]/} ]]; then max=$v; fi
+	done
+	if [[ $max = 0 ]]; then echo ""; else echo "$max"; fi
+}
+
+# Get largest version (Just compatible with my way of getting versions code)
+get_latest_version() {
+    grep -Evi 'alpha|beta' | grep -oPi '\b\d+(\.\d+)+(?:\-\w+)?(?:\.\d+)?(?:\.\w+)?\b' | max
+}
+
 uptodown() {
     config_file="./apps/uptodown/$1.json"
     name=$(jq -r '.name' "$config_file")
@@ -50,3 +74,6 @@ uptodown() {
         echo "Version $version not found or no suitable APK found."
     fi
 }
+
+uptodown "youtube"
+uptodown "youtube-music"
