21bc7fd49cacc19d10962f6aee8ed5d8c2d01b61
manhduonghn <manhduonghn@live.com>
2024-11-18 20:33:16 +0700

    Test again

--- test.sh
+++ test.sh
@@ -14,30 +14,26 @@ req() {
 
 # Hàm trích xuất href thoả mãn điều kiện
 extract_filtered_links() {
+    # Truyền các điều kiện vào thông qua các tham số
     dpi=$1
     arch=$2
     type=$3
 
     awk -v dpi="$dpi" -v arch="$arch" -v type="$type" '
     BEGIN { link = ""; dpi_found = 0; arch_found = 0; type_found = 0; printed = 0 }
-    
-    # Match the anchor tag <a class="accent_color">
+    # Trích xuất href khi gặp thẻ <a class="accent_color">
     /<a class="accent_color"/ {
         if (match($0, /href="([^"]+)"/, arr)) {
             link = arr[1]
         }
     }
-
-    # Match the dpi (e.g., nodpi) condition
-    dpi && $0 ~ ("table-cell.*\\b" dpi "\\b") { dpi_found = 1 }
-
-    # Match the arch (e.g., x86) condition
-    arch && $0 ~ ("table-cell.*\\b" arch "\\b") { arch_found = 1 }
-
-    # Match the type (e.g., APK) condition
-    type && $0 ~ ("<span class=\"apkm-badge\">" type "<\\/span>") { type_found = 1 }
-
-    # Print the link if all conditions are met
+    # Kiểm tra điều kiện "dpi"
+    dpi && $0 ~ ("table-cell.*" dpi) { dpi_found = 1 }
+    # Kiểm tra điều kiện "arch"
+    arch && $0 ~ ("table-cell.*" arch) { arch_found = 1 }
+    # Kiểm tra điều kiện "type"
+    type && $0 ~ ("<span class=\"apkm-badge\">" type) { type_found = 1 }
+    # Khi cả ba điều kiện được thỏa mãn và chưa in link, in ra và thoát
     dpi_found && arch_found && type_found && !printed {
         print link
         printed = 1
