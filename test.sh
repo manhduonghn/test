7a63b0359d02084c2a6e0cafc9279429aff69a11
manhduonghn <manhduonghn@live.com>
2024-11-18 22:00:51 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -41,6 +41,26 @@ extract_filtered_links() {
     '
 }
 
+# Read highest supported versions from Revanced 
+get_supported_version() {
+    package_name=$1
+    output=$(java -jar revanced-cli*.jar list-versions -f "$package_name" patch*.rvp)
+    version=$(echo "$output" | tail -n +3 | sed 's/ (.*)//' | grep -v -w "Any" | max | xargs)
+    echo "$version"
+}
+
+# Download necessary resources to patch from Github latest release 
+download_resources() {
+    for repo in revanced-patches revanced-cli ; do
+        githubApiUrl="https://api.github.com/repos/revanced/$repo/releases/latest"
+        page=$(req - 2>/dev/null $githubApiUrl)
+        assetUrls=$(echo $page | jq -r '.assets[] | select(.name | endswith(".asc") | not) | "\(.browser_download_url) \(.name)"')
+        while read -r downloadUrl assetName; do
+            req "$assetName" "$downloadUrl" 
+        done <<< "$assetUrls"
+    done
+}
+
 # Find max version
 max() {
 	local max=0
@@ -80,5 +100,6 @@ apkmirror() {
     req $name-v$version.apk $url
 }
 
+download_resources
 apkmirror "youtube"
 apkmirror "youtube-music"
