d28f5b5830ef9ede98a084b64f9ce2bdc30f0b6c
manhduonghn <manhduonghn@live.com>
2024-11-19 12:14:47 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ req() {
 }
 
 url="https://apkpure.net/youtube-music/com.google.android.apps.youtube.music/versions"
-url=$(req - $url | grep '"ver-item-n"')
+url=$(req - $url | grep -oP 'data-dt-version="\K[^"]*')
 echo $url
 exit
 
