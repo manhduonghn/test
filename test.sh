272992fb48b65c1ccb5976fee061a135f547fc9e
manhduonghn <manhduonghn@live.com>
2024-11-20 20:45:07 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# Script make by Mạnh Dương
 
+# Make requests like send from Firefox Android 
 req() {
     wget --header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0" \
          --header="Content-Type: application/octet-stream" \
@@ -45,6 +47,93 @@ download_resources() {
     done
 }
 
+# Filtered key words to extract link
+extract_filtered_links() {
+    local dpi="$1" arch="$2" type="$3"
+    awk -v dpi="$dpi" -v arch="$arch" -v type="$type" '
+    BEGIN { block = ""; link = ""; found_dpi = found_arch = found_type = printed = 0 }
+    /<a class="accent_color"/ {
+        if (printed) next
+        if (block != "" && link != "" && found_dpi && found_arch && found_type && !printed) { 
+            print link; printed = 1 
+        }
+        block = $0; found_dpi = found_arch = found_type = 0
+        if (match($0, /href="([^"]+)"/, arr)) link = arr[1]
+    }
+    { if (!printed) block = block "\n" $0 }
+    /table-cell/ && $0 ~ dpi { found_dpi = 1 }
+    /table-cell/ && $0 ~ arch { found_arch = 1 }
+    /apkm-badge/ && $0 ~ (">" type "</span>") { found_type = 1 }
+    END {
+        if (block != "" && link != "" && found_dpi && found_arch && found_type && !printed)
+            print link
+    }
+    '
+}
+
+# Get some versions of application on APKmirror pages 
+get_apkmirror_version() {
+    grep -oP 'class="fontBlack"[^>]*href="[^"]+"\s*>\K[^<]+' | sed 20q | awk '{print $NF}'
+}
+
+# Best but sometimes not work because APKmirror protection 
+apkmirror() {
+    config_file="./apps/apkmirror/$1.json"
+    org=$(jq -r '.org' "$config_file")
+    name=$(jq -r '.name' "$config_file")
+    type=$(jq -r '.type' "$config_file")
+    arch=$(jq -r '.arch' "$config_file")
+    dpi=$(jq -r '.dpi' "$config_file")
+    package=$(jq -r '.package' "$config_file")
+    version=$(jq -r '.version' "$config_file")
+
+    version="${version:-$(get_supported_version "$package")}"
+    url="https://www.apkmirror.com/uploads/?appcategory=$name"
+    version="${version:-$(req - $url | get_apkmirror_version | get_latest_version)}"
+    url="https://www.apkmirror.com/apk/$org/$name/$name-${version//./-}-release"
+    url="https://www.apkmirror.com$(req - "$url" | extract_filtered_links "$dpi" "$arch" "$type")"
+    url="https://www.apkmirror.com$(req - "$url" | grep -oP 'class="[^"]*downloadButton[^"]*"[^>]*href="\K[^"]+')"
+    url="https://www.apkmirror.com$(req - "$url" | grep -oP 'id="download-link"[^>]*href="\K[^"]+')"
+    req $name-v$version.apk $url
+}
+
+# X not work (maybe more)
+uptodown() {
+    config_file="./apps/uptodown/$1.json"
+    name=$(jq -r '.name' "$config_file")
+    package=$(jq -r '.package' "$config_file")
+    version=$(jq -r '.version' "$config_file")
+    version="${version:-$(get_supported_version "$package")}"
+    url="https://$name.en.uptodown.com/android/versions"
+    version="${version:-$(req - 2>/dev/null $url | grep -oP 'class="version">\K[^<]+' | get_latest_version)}"
+
+    # Fetch data_code
+    data_code=$(req - "$url" | grep 'detail-app-name' | grep -oP '(?<=data-code=")[^"]+')
+
+    page=1
+    while :; do
+        json=$(req - "https://$name.en.uptodown.com/android/apps/$data_code/versions/$page" | jq -r '.data')
+        
+        # Exit if no valid JSON or no more pages
+        [ -z "$json" ] && break
+        
+        # Search for version URL
+        version_url=$(echo "$json" | jq -r --arg version "$version" '[.[] | select(.version == $version and .kindFile == "apk")][0].versionURL // empty')
+        if [ -n "$version_url" ]; then
+            download_url=$(req - "${version_url}-x" | grep -oP '(?<=data-url=")[^"]+')
+            [ -n "$download_url" ] && req "$name-v$version.apk" "https://dw.uptodown.com/dwn/$download_url" && break
+        fi
+        
+        # Check if all versions are less than target version
+        all_lower=$(echo "$json" | jq -r --arg version "$version" '.[] | select(.kindFile == "apk") | .version | select(. < $version)' | wc -l)
+        total_versions=$(echo "$json" | jq -r '.[] | select(.kindFile == "apk") | .version' | wc -l)
+        [ "$all_lower" -eq "$total_versions" ] && break
+
+        page=$((page + 1))
+    done
+}
+
+# Tiktok not work because not available version supported 
 apkpure() {
     config_file="./apps/apkpure/$1.json"
     name=$(jq -r '.name' "$config_file")
@@ -57,7 +146,3 @@ apkpure() {
     url=$(req - $url | grep -oP '<a[^>]*id="download_link"[^>]*href="\K[^"]*' | head -n 1)
     req $name-v$version.apk "$url"
 }
-
-download_resources
-apkpure "youtube"
-apkpure "youtube-music"
