cda39485908138d51d2d8cd1722468d29810d71b
manhduonghn <manhduonghn@live.com>
2024-11-20 14:13:05 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Hàm req để tải HTML
 req() {
     wget --header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0" \
          --header="Content-Type: application/octet-stream" \
@@ -11,53 +12,31 @@ req() {
          --keep-session-cookies --timeout=30 -nv -O "$@"
 }
 
-# Find max version
-max() {
-	local max=0
-	while read -r v || [ -n "$v" ]; do
-		if [[ ${v//[!0-9]/} -gt ${max//[!0-9]/} ]]; then max=$v; fi
-	done
-	if [[ $max = 0 ]]; then echo ""; else echo "$max"; fi
+# Hàm trích xuất href thoả mãn điều kiện
+extract_filtered_links() {
+    local dpi="$1" arch="$2" type="$3"
+    awk -v dpi="$dpi" -v arch="$arch" -v type="$type" '
+    BEGIN { block = ""; link = ""; found_dpi = found_arch = found_type = printed = 0 }
+    /<a class="accent_color"/ {
+        if (printed) next
+        if (block != "" && link != "" && found_dpi && found_arch && found_type && !printed) { 
+            print link; printed = 1 
+        }
+        block = $0; found_dpi = found_arch = found_type = 0
+        if (match($0, /href="([^"]+)"/, arr)) link = arr[1]
+    }
+    { if (!printed) block = block "\n" $0 }
+    /table-cell/ && $0 ~ dpi { found_dpi = 1 }
+    /table-cell/ && $0 ~ arch { found_arch = 1 }
+    /apkm-badge/ && $0 ~ (">" type "</span>") { found_type = 1 }
+    END {
+        if (block != "" && link != "" && found_dpi && found_arch && found_type && !printed)
+            print link
+    }
+    '
 }
 
-# Get largest version (Just compatible with my way of getting versions code)
-get_latest_version() {
-    grep -Evi 'alpha|beta' | grep -oPi '\b\d+(\.\d+)+(?:\-\w+)?(?:\.\d+)?(?:\.\w+)?\b' | max
-}
-
-# Read highest supported versions from Revanced 
-get_supported_version() {
-    package_name=$1
-    output=$(java -jar revanced-cli*.jar list-versions -f "$package_name" patch*.rvp)
-    version=$(echo "$output" | tail -n +3 | sed 's/ (.*)//' | grep -v -w "Any" | max | xargs)
-    echo "$version"
-}
-
-# Download necessary resources to patch from Github latest release 
-download_resources() {
-    for repo in revanced-patches revanced-cli ; do
-        githubApiUrl="https://api.github.com/repos/revanced/$repo/releases/latest"
-        page=$(req - 2>/dev/null $githubApiUrl)
-        assetUrls=$(echo $page | jq -r '.assets[] | select(.name | endswith(".asc") | not) | "\(.browser_download_url) \(.name)"')
-        while read -r downloadUrl assetName; do
-            req "$assetName" "$downloadUrl" 
-        done <<< "$assetUrls"
-    done
-}
-
-apkpure() {
-    config_file="./apps/apkpure/$1.json"
-    name=$(jq -r '.name' "$config_file")
-    package=$(jq -r '.package' "$config_file")
-    version=$(jq -r '.version' "$config_file")
-    url="https://apkpure.net/$name/$package/versions"
-    version="${version:-$(get_supported_version "$package")}"
-    version="${version:-$(req - $url | grep -oP 'data-dt-version="\K[^"]*' | sed 10q | get_latest_version)}"
-    url="https://apkpure.net/$name/$package/download/$version"
-    url=$(req - $url | grep -oP '<a[^>]*id="download_link"[^>]*href="\K[^"]*' | head -n 1)
-    req $name-v$version.apk "$url"
-}
-
-download_resources
-apkpure "youtube"
-apkpure "youtube-music"
+# URL cần tải
+url="https://www.apkmirror.com/apk/facebook-2/messenger/messenger-484-0-0-68-109-release/"
+link=$(req - "$url" | extract_filtered_links "" "" "BUNDLE")
+echo "https://www.apkmirror.com$link"
